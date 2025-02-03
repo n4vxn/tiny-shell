6c80e5f6826808200011c8f9348b251b5b7959ff
n4vxn <naveenov23@gmail.com>
2025-02-03 23:12:53 +0530

    Added: Basic commands

--- main.go
+++ main.go
@@ -9,31 +9,76 @@ import (
 
 func main() {
 	for {
-		fmt.Print("> ")
+		// Get the current working directory
+		location, _ := os.Getwd()
+		split := strings.Split(location, "/")
+		currLocation := split[len(split)-1]
+
+		// Print the current directory prompt
+		fmt.Printf("> %s ", currLocation)
 		reader := bufio.NewReader(os.Stdin)
 		input, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
+			continue
 		}
 
 		input = strings.TrimSpace(input)
-		fmt.Println(input)
-
 		parts := strings.Fields(input)
 
 		if len(parts) == 0 {
 			continue
 		}
 
-		cmd := fmt.Sprintf(parts[0])
-		_ = fmt.Sprint(parts[1:])
+		cmd := parts[0]
+		arg := strings.Join(parts[1:], " ")
 
 		switch cmd {
-		case "show":
+		case "crib":
+			// Change directory
+			if err := os.Chdir(arg); err != nil {
+				fmt.Fprintln(os.Stderr, "Error changing directory:", err)
+			}
+
+		case "peek":
+			// List files in the current directory
 			files, _ := os.ReadDir(".")
 			for _, file := range files {
 				fmt.Println(file.Name())
 			}
+
+		case "whereami":
+			// Print the current working directory
+			location, _ := os.Getwd()
+			fmt.Println("Current directory:", location)
+
+		case "cribup":
+			// Create a new directory
+			os.Mkdir(arg, 0755)
+
+		case "yell":
+			// Output the string
+			if len(input) > 5 {
+				fmt.Println(input[6:])
+			} else {
+				fmt.Println("Nothing to yell!")
+			}
+
+		case "new":
+			// Create a new file
+			file, err := os.Create(arg)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error creating file:", err)
+			}
+			file.Close()
+
+		case "cribdown":
+			// Remove the directory and its contents
+			if err := os.RemoveAll(arg); err != nil {
+				fmt.Fprintln(os.Stderr, "Error removing directory:", err)
+			}
+
+		default:
+			fmt.Println("Unknown command:", cmd)
 		}
 	}
 }
